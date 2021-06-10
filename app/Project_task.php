85b031ce9daa8e9df0e7f5ec276a69ca0e538ba3
Raitis Rasmanis <rasmanis.raitis@gmail.com>
2021-06-10 22:38:02 +0300

    Updates to laravel 6 + php 8

--- app/Project_task.php
+++ app/Project_task.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project_task extends Model
 {
+    /**
+     * @inheritdoc
+     */
+    protected $table = 'project_tasks';
+
   public function project(){
     return $this->belongsTo('App\Project');
   }
--- config/app.php
+++ config/app.php
@@ -163,8 +163,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
